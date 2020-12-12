048ed0d961ed17779cbb30e377e8f165ae1307a2
bgungalov <bgungalov@gmail.com>
2020-12-12 23:29:49 +0200

    added income and expense activity

---
+++ app/src/main/java/com/gungalovelectronics/budget_manager/ExpenseController.java
@@ -0,0 +1,12 @@
+package com.gungalovelectronics.budget_manager;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExpenseController extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.expense_activity);
+    }
+}
--- app/src/main/java/com/gungalovelectronics/budget_manager/MainActivity.java
+++ app/src/main/java/com/gungalovelectronics/budget_manager/MainActivity.java
@@ -2,7 +2,11 @@ package com.gungalovelectronics.budget_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAddIncome = findViewById(R.id.btnAddIncome);
+        Button btnAddExpense = findViewById(R.id.btnAddExpense);
+
+        btnAddIncome.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent incomeActivity = new Intent(getApplicationContext(), IncomeController.class);
+                startActivity(incomeActivity);
+            }
+        });
+
+        btnAddExpense.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent expenseActivity = new Intent(getApplicationContext(), ExpenseController.class);
+                startActivity(expenseActivity);
+            }
+        });
     }
 }
