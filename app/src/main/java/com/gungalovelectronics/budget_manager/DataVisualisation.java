992faba30c44ddbe9408099e39c8134123e8e05f
Jivko Uzunov <59103450+jivko25@users.noreply.github.com>
2020-12-14 19:36:11 +0200

    added main activity button in DataVisualisation

--- app/src/main/java/com/gungalovelectronics/budget_manager/DataVisualisation.java
+++ app/src/main/java/com/gungalovelectronics/budget_manager/DataVisualisation.java
@@ -2,7 +2,10 @@ package com.gungalovelectronics.budget_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DataVisualisation extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class DataVisualisation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_visualisation);
+
+        Button btnBack = findViewById(R.id.button_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent main = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(main);
+            }
+        });
     }
 }
